94317a0d696cc95918f531faae74e33fd576a4fc
Ben Ripka <Benripka@gmail.com>
2018-03-27 18:05:13 -0600

    Made a shawty tuner out of it

--- audio/audio.py
+++ audio/audio.py
@@ -8,6 +8,9 @@ from scipy.fftpack import fft
 # not sure if this next library is the way to go to find harmonics
 from scipy.signal import find_peaks_cwt
 import time
+import peakutils
+import math
+
 
 
 # Note that it'll take a bit of time to fire up!
@@ -28,7 +31,7 @@ class AudioStream(object):
         self.root = None
 
         # stream object
-        #object used as a handle on the OS audio stream,
+        # Object used as a handle on the OS audio stream,
         self.p = pyaudio.PyAudio()
         self.stream = self.p.open(
             format=self.FORMAT,
@@ -41,12 +44,14 @@ class AudioStream(object):
         #get the plots going
         self.init_plots()
         self.start_plot()
+        self.findNote()
+        self.xf = None
 
     def init_plots(self):
 
         # The X values for the plot. xf are the frequencies. Numpy is efficient.
         x = np.arange(0, 2 * self.CHUNK, 2)
-        xf = np.linspace(0, self.RATE, self.CHUNK)
+        self.xf = np.linspace(0, self.RATE, self.CHUNK)
 
         # create matplotlib figure and axes
         self.fig, (ax1, ax2) = plt.subplots(2, figsize=(15, 7))
@@ -58,8 +63,7 @@ class AudioStream(object):
 
         # semilong to be used as the frequency axis
         self.line_fft, = ax2.semilogx(
-            xf, np.random.rand(self.CHUNK), '-', lw=2)
-
+            self.xf, np.random.rand(self.CHUNK), '-', lw=2)
         # format waveform axes
         ax1.set_title('Audio Waveform')
         ax1.set_xlabel('Samples')
@@ -94,21 +98,55 @@ class AudioStream(object):
             self.line.set_ydata(data_np)
 
             # compute FFT and update line
-            yf = fft(data_int)
+            YF = fft(data_int)
+            normYF = np.abs(YF[0:self.CHUNK]) / (128 * self.CHUNK)
+
 
 
             self.line_fft.set_ydata(
-                np.abs(yf[0:self.CHUNK]) / (128 * self.CHUNK))
-
+                normYF
+                )
             # update figure canvas
             self.fig.canvas.draw()
             self.fig.canvas.flush_events()
             frame_count += 1
-
+            peaks = peakutils.indexes(normYF, thres=0.2, min_dist=0)
+            points = list(zip(normYF, self.xf))
+            if len(peaks) > 0:
+                freq_peak = int(points[min(peaks)][1])
+                self.findNote(freq_peak)
         else:
             self.fr = frame_count / (time.time() - start_time)
             print('average frame rate = {:.0f} FPS'.format(self.fr))
             self.exit_app()
+
+    def findNote(self, freq):
+        base_freq = 430
+        n = (math.log(freq)-math.log(base_freq))/math.log(2 ** (1 / 12))
+        below = False
+        if n < 0:
+            bellow = True
+            n = math.fabs(n)
+        while n >= 12:
+            n -= 12
+        notes = {
+            0 : "A",
+            1 : "A#",
+            2 : "B",
+            3 : "C",
+            4 : "C#",
+            5 : "D",
+            6 : "D#",
+            7 : "E",
+            8 : "F",
+            9 : "F#",
+            10 : "G",
+            11 : "G#"
+        }
+        note = notes.get(int(n), None)
+        cents = n - int(n)
+        print(note, " + ", round(cents, 2)*100, "¢")
+
 
     def exit_app(self):
         print('stream closed')
